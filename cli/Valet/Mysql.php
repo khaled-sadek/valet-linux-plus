0f7d07fd6f17c22d837e899bec10f9b56e5a7aee
Khaled Sadek <khaledsadek286@gmail.com>
2026-08-12 15:38:21 +0300

    fix(status): Enabled status display of services used by valet. By @Stephan-MC

--- cli/Valet/Mysql.php
+++ cli/Valet/Mysql.php
@@ -99,6 +99,14 @@ class Mysql
     }
 
     /**
+     * Mysql service status.
+     */
+    public function status(): void
+    {
+        $this->sm->printStatus($this->serviceName());
+    }
+
+    /**
      * Restart the Mysql service.
      */
     public function restart(): void
